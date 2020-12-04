53139ae4976994b45d3f53e96523703ea957f30a
dhanush <dhanushsingam@gmail.com>
2020-12-04 14:28:12 +0530

    using put request to update user info

--- projbackend/controllers/user.js
+++ projbackend/controllers/user.js
@@ -15,6 +15,41 @@ exports.getUserById = (req,res,next,id)=>{
 };
 
 exports.getUser = (req,res)=>{
-//TODO : get back here for password
+//TODO : get back here for password //GOT BACK AND DONE
+req.profile.encry_password=undefined;
+req.profile.salt= undefined;
+req.profile.createdAt= undefined;
+req.profile.updatedAt= undefined;
 return res.json(req.profile);
+}
+
+// ASSIGNMENT
+// exports.getAllUsers=(req,res)=>{
+//     User.find().exec((err,users)=>{
+//         if(err || !users){
+//             return res.status(400).json({
+//                 error:"No users found"
+//             })
+//         }
+//      res.json({users})
+//     })
+// }
+
+exports.updateUser =(req,res)=>{
+    User.findByIdAndUpdate(
+        {_id:req.profile._id},
+        {$set:req.body},
+        {new:true ,useFindAndModify:false},
+        (err,user)=>{
+            if(err){
+                return res.status(400).json({
+                    error:"You are not authorised to update this user"
+                })
+             }
+             user.salt= undefined;
+             user.encry_password=undefined
+             return(res.json(user))
+        }
+    )
+
 }
